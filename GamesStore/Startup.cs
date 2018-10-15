806a8dfe90499a8888bc9efdfd80b4fbc778c776
MohamedEldefrawy <mohamed.ahmed.aly.d@gmail.com>
2018-10-16 01:52:11 +0200

    Add project files.

---
+++ GamesStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GamesStore.Startup))]
+namespace GamesStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
