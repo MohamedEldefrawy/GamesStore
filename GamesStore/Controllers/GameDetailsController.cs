79853b4e956972ed02bcabd6b1e1472c54007aad
MohamedEldefrawy <mohamed.ahmed.aly.d@gmail.com>
2018-10-16 02:27:46 +0200

    Creating GameDetail Controller and view

---
+++ GamesStore/Controllers/GameDetailsController.cs
@@ -0,0 +1,24 @@
+﻿using GamesStore.Models;
+using System.IO;
+using System.Web.Mvc;
+
+namespace DafroGames.Controllers
+{
+    public class GameDetailsController : Controller
+    {
+        private GamesEntities db = new GamesEntities();
+        
+        [HttpGet]
+        public ActionResult Detail(int? id)
+        {
+            return View(db.Games.Find(id));
+        }
+
+        public FileContentResult fullImageUrl(string path)
+        {
+            path = Path.Combine(Server.MapPath("~/Upload/Images/") + path);
+            byte[] imgarray = System.IO.File.ReadAllBytes(path);
+            return new FileContentResult(imgarray, "image/jpg");
+        }
+    }
+}
